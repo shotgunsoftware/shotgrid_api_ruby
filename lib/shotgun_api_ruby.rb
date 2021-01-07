4ea3ab81ad02fb09c6360d20c603f62e27a16708
Denis Zaratan Pasin <zaratan@hey.com>
2021-01-07 17:13:40 +0900

    Safer eager load

--- lib/shotgun_api_ruby.rb
+++ lib/shotgun_api_ruby.rb
@@ -1,18 +1,19 @@
 # frozen_string_literal: true
 
 # zeitwerk will take care of auto loading files based on their name :)
-require "zeitwerk"
+require 'zeitwerk'
 require 'active_support/core_ext/string/inflections'
 require 'ostruct'
-require "faraday"
+require 'faraday'
 require 'json'
 
 loader = Zeitwerk::Loader.for_gem
 loader.setup # ready!
-loader.eager_load
 
 module ShotgunApiRuby
   def self.new(**args)
     Client.new(**args)
   end
 end
+
+loader.eager_load
