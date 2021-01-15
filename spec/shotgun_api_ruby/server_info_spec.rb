aae3d7780ad4f91a80950dafbaff6c4c2e80b4d3
Denis Zaratan Pasin <zaratan@hey.com>
2021-01-15 18:11:34 +0900

    ServerInfo tests

---
+++ spec/shotgun_api_ruby/server_info_spec.rb
@@ -0,0 +1,18 @@
+describe ShotgunApiRuby::ServerInfo do
+  in_context 'with vcr values' do
+    subject(:server_info) { shotgun_client.server_info }
+
+    it 'exposes the connection' do
+      expect(server_info.connection).to be_a Faraday::Connection
+    end
+
+    describe 'get server_infos' do
+      subject(:server_info_get) { server_info.get }
+
+      it 'parses the server infos from shotgun' do
+        expect(server_info_get.shotgun_version).not_to be_nil
+        expect(server_info_get.api_version).not_to be_nil
+      end
+    end
+  end
+end
