61f548d4ec232476918fbdf3303326929e8b74e7
Denis Zaratan Pasin <zaratan@hey.com>
2021-01-15 18:04:01 +0900

    Client tests

--- lib/shotgun_api_ruby/client.rb
+++ lib/shotgun_api_ruby/client.rb
@@ -1,7 +1,11 @@
 # frozen_string_literal: true
 
 module ShotgunApiRuby
+  # Main class for connection.
+  #
+  # This should be only instanciated once to re-use tokens
   class Client
+    # Faraday connection
     attr_reader :connection
 
     def initialize(auth:, site_url: nil, shotgun_site: nil)
@@ -16,14 +20,17 @@ module ShotgunApiRuby
         end
     end
 
+    # Access preferences APIs
     def preferences
       @preferences = Preferences.new(connection)
     end
 
+    # Access server_info APIs
     def server_info
       @server_info || ServerInfo.new(connection)
     end
 
+    # Access entities related APIs
     def entities(type)
       public_send(type)
     end
@@ -34,16 +41,17 @@ module ShotgunApiRuby
 
     def method_missing(name, *args, &block)
       if args.empty?
-        name = formated_name(name)
-        define_singleton_method(name) do
-          if entities_client = instance_variable_get("@#{name}")
+        fname = formated_name(name)
+        define_singleton_method(fname) do
+          if entities_client = instance_variable_get("@#{fname}")
             entities_client
           else
-            entities_client = entities_aux(name)
-            instance_variable_set("@#{name}", entities_client)
+            entities_client = entities_aux(fname)
+            instance_variable_set("@#{fname}", entities_client)
           end
         end
-        send(name)
+        class_eval { alias_method name, fname }
+        send(fname)
       else
         super
       end
