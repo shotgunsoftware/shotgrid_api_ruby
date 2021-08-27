76928dc1647a83b1002320c7ad746bb05353a36e
Denis Zaratan Pasin <zaratan@hey.com>
2021-08-27 18:28:39 +0900

    Typing Server Info

--- lib/shotgrid_api_ruby/server_info.rb
+++ lib/shotgrid_api_ruby/server_info.rb
@@ -1,14 +1,19 @@
-# typed: true
+# typed: strict
 # frozen_string_literal: true
 
 module ShotgridApiRuby
   class ServerInfo
+    extend T::Sig
+
+    sig { params(connection: Faraday::Connection).void }
     def initialize(connection)
-      @connection = connection
+      @connection = T.let(connection, Faraday::Connection)
     end
 
+    sig { returns(Faraday::Connection) }
     attr_reader :connection
 
+    sig { returns(OpenStruct) }
     def get
       resp = @connection.get
       resp_body = JSON.parse(resp.body)
