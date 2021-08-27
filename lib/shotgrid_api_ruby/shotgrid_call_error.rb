d48e0e6ec4bfa6950eb979e5123ffc7091c2e60b
Denis Zaratan Pasin <zaratan@hey.com>
2021-08-27 21:04:16 +0900

    Typing call error

--- lib/shotgrid_api_ruby/shotgrid_call_error.rb
+++ lib/shotgrid_api_ruby/shotgrid_call_error.rb
@@ -1,15 +1,18 @@
-# typed: true
+# typed: strict
 require 'forwardable'
 
 module ShotgridApiRuby
   class ShotgridCallError < StandardError
+    extend T::Sig
     extend Forwardable
 
+    sig { params(response: T.untyped, message: String).void }
     def initialize(response:, message:)
-      @response = response
+      @response = T.let(response, T.untyped)
       super(message)
     end
 
+    sig { returns(T.untyped) }
     attr_reader :response
     def_delegators :response, :status
   end
