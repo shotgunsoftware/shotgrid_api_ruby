d92b5fcb7df4eed5c51a5ffed2eca0506f764e48
Denis Zaratan Pasin <zaratan@hey.com>
2021-08-27 21:28:02 +0900

    Typing schema

--- lib/shotgrid_api_ruby/entities/schema.rb
+++ lib/shotgrid_api_ruby/entities/schema.rb
@@ -1,16 +1,29 @@
-# typed: true
+# typed: strict
 # frozen_string_literal: true
 
 module ShotgridApiRuby
   class Entities
     class Schema
+      extend T::Sig
+
+      sig do
+        params(
+          connection: Faraday::Connection,
+          type: T.any(String, Symbol),
+          base_url_prefix: URI,
+        ).void
+      end
       def initialize(connection, type, base_url_prefix)
-        @connection = connection.dup
-        @type = type
+        @connection = T.let(connection.dup, Faraday::Connection)
+        @type = T.let(type, T.any(String, Symbol))
         @connection.url_prefix = "#{base_url_prefix}/schema/#{type}"
       end
-      attr_reader :type, :connection
+      sig { returns(T.any(String, Symbol)) }
+      attr_reader :type
+      sig { returns(Faraday::Connection) }
+      attr_reader :connection
 
+      sig { returns(OpenStruct) }
       def read
         resp = @connection.get('')
 
@@ -26,6 +39,7 @@ module ShotgridApiRuby
         OpenStruct.new(resp_body['data'].transform_values { |v| v['value'] })
       end
 
+      sig { returns(OpenStruct) }
       def fields
         resp = @connection.get('fields')
         resp_body = JSON.parse(resp.body)
